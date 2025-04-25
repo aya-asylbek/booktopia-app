ba85e14d7193e89b345d0627a97fb0718fca267f
aya-asylbek <aichurek0990@gmail.com>
2025-04-25 12:58:49 -0700

    Update BookCard.jsx

--- client/src/components/BookCard.jsx
+++ client/src/components/BookCard.jsx
@@ -1,17 +1,11 @@
 import React from "react";
 
 const BookCard = ({ book }) => {
-  const volume = book.volumeInfo;
-
   return (
-    <div style={{ border: "1px solid gray", padding: "10px", margin: "10px" }}>
-      <img
-        src={volume.imageLinks?.thumbnail}
-        alt={volume.title}
-        style={{ width: "100px" }}
-      />
-      <h3>{volume.title}</h3>
-      <p>{volume.authors?.join(", ")}</p>
+    <div className="book-card">
+      <img src={book.cover} alt={book.title} />
+      <h3>{book.title}</h3>
+      <p>{book.author}</p>
     </div>
   );
 };
