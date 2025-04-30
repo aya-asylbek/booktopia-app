620a5473da348e5878af9c3ec656693c92932a5f
aya-asylbek <aichurek0990@gmail.com>
2025-04-29 23:57:01 -0700

    Added tailwind css to BookList component

--- client/src/components/BookList.jsx
+++ client/src/components/BookList.jsx
@@ -3,7 +3,7 @@ import BookCard from "./BookCard";
 
 const BookList = ({ books }) => {
   return (
-    <div className="book-list">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
       {books.map((book) => (
         <BookCard key={book.id} book={book} />
       ))}
