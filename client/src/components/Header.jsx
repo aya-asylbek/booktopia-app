db61c2047c6d886909855833a47ff1346aed2a99
aya-asylbek <aichurek0990@gmail.com>
2025-04-30 15:43:10 -0700

    Reinstalled tailwind and add ui to landing page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="text-center mb-8">
-      <h1 className="text-3xl font-bold text-gray-800">📚 Booktopia</h1>
+    <header className="text-center fixed -mt-8 w-screen h-40 mb-8 bg-sky-200">
+      <h1 className="text-3xl pt-2 font-bold text-gray-800">📚 Booktopia</h1>
     </header>
   );
 };
