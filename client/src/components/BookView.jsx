6563aa3ae27db10ef5732b7fd33c2987662791af
aya-asylbek <aichurek0990@gmail.com>
2025-05-01 11:05:55 -0700

    Changed background on BookView component from black to gray

--- client/src/components/BookView.jsx
+++ client/src/components/BookView.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const BookView = ({ book, onClose }) => {
   return (
-    <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 z-50">
+    <div className="fixed inset-0 flex justify-center items-center bg-gray-200 bg-opacity-50 z-50">
       <div className="bg-white p-6 rounded-lg shadow-xl max-w-lg w-full">
+        {/* Button to close my modal on top right */}
         <button onClick={onClose} className="absolute top-2 right-2 text-gray-600 text-lg">
           &times;
         </button>
