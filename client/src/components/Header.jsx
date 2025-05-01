04d06e72a77562aa4a4bafc6327fb67a020e2725
aya-asylbek <aichurek0990@gmail.com>
2025-04-30 19:43:13 -0700

    Added some styles to header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,10 +2,14 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="fixed top-0 left-0 p-4 bg-sky-200 z-10">
-      <h1 className="text-3xl font-bold text-gray-800">📚 Booktopia</h1>
+    <header className="fixed top-0 w-full p-4 z-20 bg-transparent">
+      <h1 className="text-3xl font-bold text-white drop-shadow-md">
+        📚 Booktopia
+      </h1>
     </header>
   );
 };
 
 export default Header;
+
+
