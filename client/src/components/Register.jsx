e0a59d07650ec49045363218741a232b9ef586c1
aya-asylbek <aichurek0990@gmail.com>
2025-05-04 10:32:10 -0700

    Added json.stringify username

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -23,7 +23,8 @@ const Register = () => {
                 headers: {
                     "Content-Type": "application/json",
                 },
-                body: JSON.stringify({ email, password }),
+                body: JSON.stringify({ username, email, password }), // added for my state username
+
             });
 
             const data = await response.json();
